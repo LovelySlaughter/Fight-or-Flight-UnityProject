710897d0255eece869beb1fd790a36c60a678594
LovelySlaughter <kathleen.rookard@gmail.com>
2023-01-08 04:51:40 -0500

    Update cameraControls.cs

--- Fight or Flight-V3/Assets/Scripts/cameraControls.cs
+++ Fight or Flight-V3/Assets/Scripts/cameraControls.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class cameraControls : MonoBehaviour
 {
+    [Header("--- Camera Stats ---")]
+    [SerializeField] int ySensitivity;
+    [SerializeField] int xSensitivity;
+    float xRotation;
+    float yRotation;
+
+    [Header("--- Clamp Stats ---")]
+    [SerializeField] int verticalMax;
+    [SerializeField] int verticalMin;
+
+    bool pilotControls;
+    bool invertY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseHorizontal = Input.GetAxis("Horizantal") * Time.deltaTime * xSensitivity;
+        float mouseVertical = Input.GetAxis("Vertical") * Time.deltaTime * ySensitivity;
+
+        if (pilotControls)
+        {
+            xRotation += mouseVertical;
+        }
+        else
+        {
+            xRotation -= mouseVertical;
+        }
+        if (invertY)
+        {
+            yRotation += mouseHorizontal;
+        }
+        else
+        {
+            yRotation -= mouseHorizontal;
+        }
+
+
+       
     }
 }
