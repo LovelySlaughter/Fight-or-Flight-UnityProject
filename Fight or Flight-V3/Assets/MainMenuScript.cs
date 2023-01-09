f53e03f455efcf02acb39d0fb964da969af51db3
LovelySlaughter <kathleen.rookard@gmail.com>
2023-01-09 09:11:03 -0500

    Main Menu
    Still just, working on the main menu

--- Fight or Flight-V3/Assets/MainMenuScript.cs
+++ Fight or Flight-V3/Assets/MainMenuScript.cs
@@ -1,12 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject options;
     public void NewGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OpenSettings()
+    {
+        mainMenu.SetActive(false);
+        options.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        options.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void OpenLoad()
+    {
+
+    }
+    public void CloseLoad()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quitting...");
+        Application.Quit();
     }
 }
