8c9d84c808faa962766a43fb2147171b1dc3c172
LovelySlaughter <kathleen.rookard@gmail.com>
2023-01-09 08:32:28 -0500

    Build manager setup
    Setting up build Manager for main menu and level flow

--- Fight or Flight-V3/Assets/MainMenuScript.cs
+++ Fight or Flight-V3/Assets/MainMenuScript.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
     public void NewGame()
     {
-        Scen
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
