e48527a19197a0e775e5fdfc40b9347d717243ed
LovelySlaughter <mkrookard@student.fullsail.edu>
2023-01-09 22:57:14 -0500

    LevelUpdae(Kat)

--- Fight or Flight-V3/Assets/Scripts/playerController.cs
+++ Fight or Flight-V3/Assets/Scripts/playerController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// Coded by Kat
 public class playerController : MonoBehaviour
 {
     [Header("--- Character Components ---")]
