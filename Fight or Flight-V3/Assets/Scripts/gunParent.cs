0f6bd5f760c03169633e8e35a81f2cfdc94cc1fd
LovelySlaughter <kathleen.rookard@gmail.com>
2023-01-11 01:10:04 -0500

    commiting

---
+++ Fight or Flight-V3/Assets/Scripts/gunParent.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public interface gunParent 
+{
+    //   float shootRate;
+    //   int shootDamage;
+    //  int bulletSpeed;
+    void selectGun(int x);
+
+
+}
