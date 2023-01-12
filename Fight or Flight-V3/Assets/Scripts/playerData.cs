9e5fa248f6c79a4bef1b83635daa51da1abe88f5
LovelySlaughter <mkrookard@student.fullsail.edu>
2023-01-11 20:42:59 -0500

    ui

--- Fight or Flight-V3/Assets/Scripts/playerData.cs
+++ Fight or Flight-V3/Assets/Scripts/playerData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Coded by Kat
 public class playerData : MonoBehaviour
 {
     // Start is called before the first frame update
