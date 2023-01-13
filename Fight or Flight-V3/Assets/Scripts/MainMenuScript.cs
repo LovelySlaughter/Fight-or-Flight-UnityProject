d76da466e4b2eec3651f30171fbdac1bf4a7b0c5
LovelySlaughter <mkrookard@student.fullsail.edu>
2023-01-12 19:56:44 -0500

    update and import

--- Fight or Flight-V3/Assets/Scripts/MainMenuScript.cs
+++ Fight or Flight-V3/Assets/Scripts/MainMenuScript.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.VersionControl;
+//using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
