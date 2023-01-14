f73453b97f38a654d8c4a049b50d42384f1d5a65
LovelySlaughter <mkrookard@student.fullsail.edu>
2023-01-13 23:34:29 -0500

    Update basicWeapon.cs

--- Fight or Flight-V3/Assets/Scripts/basicWeapon.cs
+++ Fight or Flight-V3/Assets/Scripts/basicWeapon.cs
@@ -5,35 +5,64 @@ using UnityEngine;
 public class basicWeapon : MonoBehaviour
 {
     [Header("--- Gun Components ---")]
-    [SerializeField] GameObject weapon;
     [SerializeField] GameObject bullets;
-    [SerializeField] int rateOfFire;
-    [SerializeField] int damage;
-    [SerializeField] int shootRange;
 
+    [Header("--- Gun Stats ---")]
+    public int rateOfFire;
+    public float spread;
+    public float reloadTime;
+    public float timeBetweenShots;
+
+    public int magazineSize;
+    public int bulletsPerTap;
     public bool automatic;
-    public bool burst;
-    public int burstTime;
-    public int burstRate;
-    public bool scoped;
-    public int scopedTime;
-    bool ADS;
-    public int swayRate;
+
+    int bulletsRemaining;
+    int bulletsFired;
+
+    public int damage;
+    public int shootRange;
+
+    public float shootForce;
+    public float upwardForce;
 
 
-    int magazine;
+    // Bools
+    bool isShooting;
+    bool readyToShoot;
+    bool reloading;
 
+    // References
+    [SerializeField] Camera cam;
+    [SerializeField] Transform shootPOS;
+
+    public bool allowInvoke = true;
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-
+        // magazine must be full
+        bulletsRemaining = magazineSize;
+        readyToShoot = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        MyInput();
+    }
 
+    private void MyInput()
+    {
+        // check if gun is automatic
+        if (automatic)
+        {
+            isShooting = Input.GetKey(KeyCode.Mouse0);
+        }
+        else
+        {
+            isShooting = Input.GetKeyDown(KeyCode.Mouse0);
+        }
     }
 }
