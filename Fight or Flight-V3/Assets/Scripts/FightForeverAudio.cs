509d6daf319e481613ae4d0a147bc94ca851bcf4
LovelySlaughter <mkrookard@student.fullsail.edu>
2023-01-22 08:11:38 -0500

    Import, Organize and Update

---
+++ Fight or Flight-V3/Assets/Scripts/FightForeverAudio.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightForeverAudio : MonoBehaviour
+{
+    [SerializeField] AudioSource sounds;
+    [SerializeField] AudioClip[] music;
+    [Range(0, 1)][SerializeField] float musicVolume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < music.Length; i++)
+        {
+            sounds.PlayOneShot(music[Random.Range(0, music.Length - 1)], musicVolume);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
