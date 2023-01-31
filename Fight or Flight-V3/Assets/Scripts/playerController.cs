345ad6733af4494af50c1c29cf9999613cc1ac62
GarciaMauricio <mg5757540@gmail.com>
2023-01-30 22:01:10 -0600

    Update Final

--- Fight or Flight-V3/Assets/Scripts/playerController.cs
+++ Fight or Flight-V3/Assets/Scripts/playerController.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
-using UnityEditor.Animations;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.UIElements;
 
